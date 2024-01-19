149522078765badd434b155bb6489bf54bd5235d
hanzalah <hanzalahimran9@gmail.com>
2024-01-20 02:52:03 +0500

    Dark and Light mode themes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import {
+  ThemeProvider,
+  createTheme,
+  PaletteMode,
+  Switch,
+  Typography,
+  Box,
+} from '@mui/material';
+import { useState } from 'react';
+
 function App() {
+  const [themeMode, setThemeMode] = useState<PaletteMode>('light');
+  const theme = createTheme({
+    palette: {
+      mode: themeMode,
+    },
+  });
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello Kati and Jami!</h1>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="flex justify-center ">
+        <Box className="flex flex-col justify-center items-center">
+          <Typography variant="h4" component="h2">
+            Toggle {themeMode === 'light' ? 'Dark' : 'Light'} Mode
+          </Typography>
+          <Switch
+            aria-label="themeToggle"
+            defaultChecked
+            size="small"
+            onChange={() =>
+              setThemeMode(themeMode === 'light' ? 'dark' : 'light')
+            }
+          />
+        </Box>
+      </div>
+    </ThemeProvider>
   );
 }
 
