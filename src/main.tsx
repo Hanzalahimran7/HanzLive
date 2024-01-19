c3dc1ce44a3d5e5286996699d82df4021837a861
hanzalah <hanzalahimran9@gmail.com>
2024-01-20 02:54:35 +0500

    Removed unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { ThemeProvider, createTheme, PaletteMode } from '@mui/material';
-import { useState } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
